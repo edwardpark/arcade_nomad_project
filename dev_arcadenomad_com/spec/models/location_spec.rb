e733a5601022c39bdf3432c785e28170692ae915
Edward Park <parkes.edward@gmail.com>
2015-07-19 18:16:39 -0400

    added RSpec test for location model

--- dev_arcadenomad_com/spec/models/location_spec.rb
+++ dev_arcadenomad_com/spec/models/location_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it " can be instatiated " do #example of what tests are intended to cover
+    location = Location.new
+    expect(location).to be_a Location
+  end #end of it " can be instatiated"
+
+end #end of RSpec
